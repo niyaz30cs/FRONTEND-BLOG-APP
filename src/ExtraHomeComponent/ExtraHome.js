117ee4ebfd07e5a24ff36868a126c7cfcce84cb9
Niyaz Alam <110074633+niyaz30cs@users.noreply.github.com>
2023-10-29 09:55:03 +0530

    updated

--- src/ExtraHomeComponent/ExtraHome.js
+++ src/ExtraHomeComponent/ExtraHome.js
@@ -2,25 +2,29 @@ import React, { useContext } from 'react'
 import { Link } from 'react-router-dom';
 import { MyContext } from '../DataStore/DataPackage'
 import Bollywood from '../Component/Bollywood';
+// import "react-slideshow-image/dist/styles.css";
+// import { Fade, Zoom, Slide } from "react-slideshow-image"
 
 function ExtraHome() {
-  const[extraData]=useContext(MyContext);
+  const [extraData] = useContext(MyContext);
   return (
     <>
-            {
-        extraData.filter((item)=>item.category==="Bollywood" && item.id===33).map((value,index)=>{
-            return(
-                <div key={index} className='Extra'>
-                    <img src={value.urlToImage} alt='look3' className='extraImage'/>
-                    <div className='extraContent'>
-                    <Link to={`/details/${Bollywood.title}`} state={value} className='removeLine'><h3>{value.title}</h3></Link>
-                    <h4>{value.description}</h4>
-                    <p>{value.publishedAt}</p>
-                    </div>
+      {/* <Fade> */}
+        {
+          extraData.filter((item) => item.category === "Bollywood" && item.id===33).map((value, index) => {
+            return (
+              <div key={index} className='Extra'>
+                <img src={value.urlToImage} alt='look3' className='extraImage' />
+                <div className='extraContent'>
+                  <Link to={`/details/${Bollywood.title}`} state={value} className='removeLine'><h3>{value.title}</h3></Link>
+                  <h4>{value.description}</h4>
+                  <p>{value.publishedAt}</p>
                 </div>
+              </div>
             )
-        })
-      }
+          })
+        }
+      {/* </Fade> */}
     </>
   )
 }
